7bdd133988be3a2eabdafe8a3c2923382b33dc3e
protokopf <protokopf@gmail.com>
2016-05-12 21:47:09 +0300

    c

---
+++ OrganizerCore/View/WindowTypes/LabelWindow.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OrganizerCore.View.ExtraTypes;
+
+namespace OrganizerCore.View.WindowTypes
+{
+    class RunningStringWindow : BasicWindow, Drawable
+    {
+        private const string mDefaultString = "There is no messages";
+        private string Label { get; set; }
+
+        private void MoveStringAlongWindow()
+        {
+
+        }
+
+
+        public RunningStringWindow() : base()
+        {
+
+        }
+
+        void Drawable.Draw()
+        {
+            // тут будет отрисовка строки в середине окна
+        }
+        void Drawable.Clean()
+        {
+            // тут строка будет очищаться
+        }
+        bool Drawable.IsChanged()
+        {
+            return base.IsChanged();
+        }
+
+        public override void KeyReact(ConsoleKeyInfo key, ref BasicWindow activeWindow)
+        {
+            switch(key.Key)
+            {
+                case ConsoleKey.Enter:
+                    Label = mDefaultString;
+                    break;
+            }
+        }
+        public override void Action()
+        {
+            MoveStringAlongWindow();
+            this.IsWindowChanged = true;
+        }
+    }
+}
